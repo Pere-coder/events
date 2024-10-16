450c4c2dc5aa96db98bd46b5a6d197dd16ff7a83
Pere-coder <gpere800@gmail.com>
2024-10-16 08:40:19 +0100

    edits to events filter and order by

--- app1/models.py
+++ app1/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Events(models.Model):
     description = models.TextField(null=True, max_length=1000)
     link = models.URLField(null=True)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.description
--- app1/views.py
+++ app1/views.py
@@ -2,14 +2,22 @@ from django.shortcuts import render
 from .models import Events
 from .serializers import EventsSerializer
 from rest_framework.generics import  ListCreateAPIView, RetrieveUpdateDestroyAPIView
-
+from django.utils import timezone
 # Create your views here.
 
 class EventsListCreateAPIView(ListCreateAPIView):
-    queryset = Events.objects.all()
     serializer_class = EventsSerializer
+
+    def get_queryset(self):
+        now = timezone.now()
+        return Events.objects.filter(date__gte=now).order_by('-date')
+    
+
+    
 
 class EventsDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Events.objects.all()
     serializer_class = EventsSerializer
+
+
 
